9288a63e40ad42085eba32de37e764194389958f
C.Lee Taylor <leet@tari.com>
2025-04-15 16:43:45 +0200

    chore(ci): revert change

--- applications/minotari_merge_mining_proxy/src/common/proxy.rs
+++ applications/minotari_merge_mining_proxy/src/common/proxy.rs
@@ -26,7 +26,6 @@ use std::convert::TryInto;
 
 use bytes::BytesMut;
 use futures::StreamExt;
-use futures_util::StreamExt;
 use hyper::{header, header::HeaderValue, http::response, Body, Response, StatusCode, Version};
 use reqwest::{ResponseBuilderExt, Url};
 use serde_json as json;
